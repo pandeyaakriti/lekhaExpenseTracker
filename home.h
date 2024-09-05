26c8da3eba72aae7a6775ccd023f429ff39da91d
Aakriti Pandey <aakriti.pandeyyy@gmail.com>
2024-09-05 13:09:40 +0545

    home.h uploaded and updated

---
+++ home.h
@@ -0,0 +1,72 @@
+#ifndef HOME_H
+#define HOME_H
+
+#include "analytics.h"
+#include <QMainWindow>
+#include <QComboBox>
+#include <QDialog>
+#include <QDialogButtonBox>
+#include <QCheckBox>
+#include <QDialog>
+#include <QMainWindow>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QApplication>
+#include <QMessageBox>
+#include <QString>
+#include <QWidget>
+#include <QMessageBox>
+#include <QDebug>
+#include <QLabel>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+
+namespace Ui {
+class Home;
+}
+
+class Home : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit Home(QWidget *parent = nullptr);
+    static void callAvailableBalance();
+    ~Home();
+
+private slots:
+
+    void on_lendButton_clicked();
+
+    void on_borrowButton_clicked();
+
+    void on_analyticsButton_clicked();
+
+    void deleteItem(int id);
+    void borrowResolved(int id, double amount);
+
+
+    void on_addItemButton_clicked();
+
+    void on_pushButton_clicked();
+
+private:
+    Ui::Home *ui;
+    QMap<QFrame*, int> frameToIdMap;
+    Analytics *analyticsWindow;
+    QSqlDatabase db;
+    bool validateInput(QString amount, QString description);
+    bool validateInput(int &amount, QString &description, QString &category, QString &payMode);
+    void insertData(int amount, const QString &description, const QString &category, const QString &payMode);
+    void insertData(int amount, const QString &description, const QString &tag);
+    void displayStatement();
+    void notifyLendBorrow();
+    void deleteItem();
+    void showAvailableBalance();
+    void createTables();
+    int currentMonth, currentYear;
+};
+
+
+#endif // HOME_H
